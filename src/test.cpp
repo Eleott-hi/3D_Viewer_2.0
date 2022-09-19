26c96539ba170068885a76a85bdbbd21082b637a
pintoved <pintoved@student.21-school.ru>
2022-09-19 22:10:31 +0700

    start

---
+++ src/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class FileReader
+{
+private:
+    /* data */
+public:
+ FileReader(){}
+     ~FileReader(){}
+};
+
+
+int main(int argc, char const *argv[]) {
+    FileReader fileReader_;
+  std::cout << "Hello world" << std::endl;
+  return 0;
+}
